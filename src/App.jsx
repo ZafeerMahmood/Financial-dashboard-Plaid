e797da27a8f2dd47427cf91d3befde417a8b28d5
Zafeer Mahmood <zafeer7460@gmail.com>
2023-04-27 16:08:40 +0500

    stops from relinking same bank with onSuccess()

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,88 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { usePlaidLink } from "react-plaid-link";
-import {getExpireDate } from "./components/components";
-import {useCookies} from 'react-cookie'
+import { getExpireDate } from "./components/components";
+import { useCookies } from "react-cookie";
+import {toast} from 'react-toastify';
 
 const App = () => {
-
   const [cookies, setCookie] = useCookies(['Email']);
   const [linkToken, setLinkToken] = useState(null);
-  const [email,setEmail] = useState(null);
-  
+  const email = useRef();
   const generateToken = async () => {
-    const item = localStorage.getItem("token");
-    if (item) {
-
-      setLinkToken(JSON.parse(item).value.link_token);
+    const token = cookies.token
+    if (token) {
+      setLinkToken(token);
       return;
-
     } else {
-        const response = await fetch("api/linkToken", {
+        const response = await fetch("/api/linkToken", {
           method: "GET",
         });
-       
         const data = await response.json();
-        localStorage.setItem( "token",JSON.stringify({
-            value: data,
-            expires: getExpireDate(),
-          })
-        );
-
+      setCookie('token',data.link_token, { path: '/' ,expires: getExpireDate()});
       setLinkToken(data.link_token);
     }
   };
 
   useEffect(() => {
+    if(cookies.email){
+      email.current = cookies.email;
+    }
+    else {
+      setCookie('email','zafeer746@gmail.com', { path: '/' ,expires: getExpireDate()});
+    }
     generateToken();
   }, []);
 
   const { open, ready } = usePlaidLink({
     token: linkToken,
-    onSuccess: (public_token) => {
-      fetch("/api/setAccessToken", {
+    onSuccess: async (public_token, metadata) => {
+      email.current = cookies.email;
+      console.log(email,'inside Onsuccess');
+      const id = metadata.institution.institution_id;
+      const response = await fetch("/api/accounts", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         },
-        body: `public_token=${public_token}&email=${cookies.email}`,
-      }).then((res) => {
-        console.log(res.body, "Token set successfully");
-      });
-    },
-  });
+        body: `email=${email.current}`,
+      })
+
+      const data = await response.json();
+      if(Object.keys(data).length!==0){
+        console.log('inside if')
+        data.forEach(data => {
+          if(data===id){
+            console.log('account already linked')
+            toast('Account already linked');
+            return;
+          }else{
+            fetch("/api/setAccessToken", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                },
+              body: `public_token=${public_token}&email=${email.current}`,
+            }); 
+          }}) 
+        }
+        else {
+        fetch("/api/setAccessToken", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                  },
+                body: `public_token=${public_token}&email=${email.current}`,
+              }); 
+        }         
+    },//End of onSuccessFunction
+  });//End of UsePlaidLinkFunction
 
   return (
     <div className="App">
-      {/* <button onClick={() => open()} disabled={!ready}>
+      <button onClick={() => open()} disabled={!ready}>
         Link account
-      </button> */}
+      </button>
     </div>
   );
 };
--- src/components/components.js
+++ src/components/components.js
@@ -8,8 +8,10 @@
   // funtion to to make a expirate date of 1 hour and return it
     const getExpireDate = () => {
     const date = new Date();
-    date.setTime(date.getTime() + (1 * 60 * 60 * 1000));
+    date.setTime(date.getTime() + (50 * 60 * 1000));
     return date;
     }
+
+  
 
   export { generateUid,getExpireDate };
